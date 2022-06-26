52872189d58a22bbe53e9fdb77613f1cea92cc2f
Werefox <jack@wrightus.com>
2022-06-26 17:59:55 -0400

    Implemented jump to nav button

--- src/components/NavButtons.js
+++ src/components/NavButtons.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { nextId, prevId, setId } from '../features/dataSlice'
 
 function NavButtons() {
 	const dispatch = useDispatch()
+	const [input, setInput] = useState('')
+
+	const goToId = (e) => {
+		e.preventDefault()
+		dispatch(setId(input))
+	}
 
 	return (
 		<div>
-			<button onClick={() => dispatch(prevId())}>
-				Previous
-			</button>
+			<div>
+				<button onClick={() => dispatch(prevId())}>
+					Previous
+				</button>
 
-			<button onClick={() => dispatch(nextId())}>
-				Next
-			</button>
+				<button onClick={() => dispatch(nextId())}>
+					Next
+				</button>
+			</div>
+			<div>
+				<form onSubmit={(e) => goToId(e)}>
+					<input 
+						type="text"
+						onChange={(e) => setInput(e.target.value)}
+					/>
+					<button type="submit">
+						Go to...
+					</button>
+				</form>
+			</div>
 		</div>
 	)
 }
